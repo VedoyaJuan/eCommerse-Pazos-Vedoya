62ba150af3e044dd902b69d0b4918f5c5f0490ae
Vedoya Juan <juanpa.vedoya@gmail.com>
2026-04-12 18:59:53 -0300

    Include cache files for Vercel and make bootstrap/cache writable

---
+++ bootstrap/cache/config.php
@@ -0,0 +1,8 @@
+<?php
+// Configuration cache file
+// This file is regenerated dynamically if it doesn't exist
+// It serves as a fallback for Vercel serverless environment
+
+return [
+    // Empty config - Laravel will load from individual config files
+];
