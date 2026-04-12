ae32d1cd701b54ab9e3e4720d2f30e8d3b696f4a
Vedoya Juan <juanpa.vedoya@gmail.com>
2026-04-12 19:21:09 -0300

    fix: remove empty config cache that broke Laravel on Vercel

--- bootstrap/cache/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration cache file
-// This file is regenerated dynamically if it doesn't exist
-// It serves as a fallback for Vercel serverless environment
-
-return [
-    // Empty config - Laravel will load from individual config files
-];
--- public/index.php
+++ public/index.php
@@ -5,12 +5,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Configure for Vercel serverless environment
-if (getenv('VERCEL')) {
-    // Use bootstrap cache config file that always exists
-    putenv('APP_CONFIG_CACHE=' . __DIR__ . '/../bootstrap/cache/config.php');
-}
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
