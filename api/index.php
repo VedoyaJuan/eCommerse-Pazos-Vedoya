9e7b343ce7b0185d8c54de96d716d1cd74e346b7
Vedoya Juan <juanpa.vedoya@gmail.com>
2026-05-08 17:31:59 -0300

    configuracion para despliegue en vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
 if (getenv('VERCEL')) {
     putenv('APP_CONFIG_CACHE=' . __DIR__ . '/../bootstrap/cache/config.php');
     putenv('LOG_CHANNEL=stderr');
